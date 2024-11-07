6632cefdfa3e189192f8ef3aadfb3e3bedd66f32
Mátics Dániel <mcdani918@gmail.com>
2024-11-07 15:13:19 +0100

    Frames and speech bubbles

---
+++ ProjektLavor/ViewModels/CollectionBrowserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ProjektLavor.Commands;
+using ProjektLavor.Services;
+using ProjektLavor.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace ProjektLavor.ViewModels
+{
+    public class CollectionBrowserViewModel : ViewModelBase
+    {
+        public List<Image> Items { get; set; }
+
+        public ICommand CloseModalCommand { get; set; }
+        public ICommand OkModalCommand { get; set; }
+
+        public CollectionBrowserViewModel(ProjectStore projectStore, INavigationService navigationService, List<Image>? items = null)
+        {
+            Items = items ?? new List<Image>();
+
+            CloseModalCommand = new NavigateCommand(navigationService);
+            OkModalCommand = new NavigateCommand(navigationService);
+
+            OnPropertyChanged(nameof(Items));
+        }
+    }
+}
